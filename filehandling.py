8f49a2d74229572d1b2d3016851323bfec54af80
Syntheticz <guiangphilip08@gmail.com>
2023-07-04 22:35:11 +0800

    File Handling With Encryption

--- filehandling.py
+++ filehandling.py
@@ -1,12 +1,26 @@
-from os import path
+from os import path, makedirs
 from objects import Account as ACCOUNT
 
 FILENAME = "Records.txt"
+FILEPATH = ["ATM", "ADMIN"]
+KEY =5
+
+def saveAccount(account):
+    encrypt_account(account, KEY)
+    
+    for PATH in FILEPATH:
+        if not path.exists(PATH):
+            makedirs(PATH)
+        if not path.isfile(f"{PATH}/{FILENAME}"):
+             with open(f"{PATH}/{FILENAME}", "w") as file:
+                pass
+        save(f"{PATH}/{FILENAME}", account)
+    
 
 def save(filepath: str, account: ACCOUNT):
     if not path.isfile(filepath):
         with open(filepath, "w") as file:
-            print("File Created")
+            pass
 
     existing_account = retrieve_by_account_number(filepath, account.account_number)
     if existing_account is not None:
@@ -15,12 +29,11 @@ def save(filepath: str, account: ACCOUNT):
     else:
         # Append the new account to the end of the file
         with open(filepath, "a") as file:
-            acc_str = f"{account.to_csv()} \n"
+            acc_str = f"{account.to_csv()}\n"
             file.write(acc_str)
 
 def retrieve_by_account_number(filepath: str, account_number: str):
     if not path.isfile(filepath):
-        print("File does not exist")
         return None
 
     with open(filepath, "r") as file:
@@ -29,8 +42,8 @@ def retrieve_by_account_number(filepath: str, account_number: str):
             account = ACCOUNT.from_csv(csv_string)
             if account.account_number == account_number:
                 return account
-
-    return None
+        return None
+    
 
 def overwrite_record(filepath: str, existing_account: ACCOUNT, new_account: ACCOUNT):
     lines = []
@@ -41,13 +54,48 @@ def overwrite_record(filepath: str, existing_account: ACCOUNT, new_account: ACCO
             csv_string = line.strip()
             account = ACCOUNT.from_csv(csv_string)
             if account.account_number == existing_account.account_number:
-                lines.append(new_account.to_csv())
+                acc_str = new_account.to_csv()
+                lines.append(acc_str)
                 updated = True
             else:
                 lines.append(csv_string)
 
+    lines[len(lines) - 1] += "\n"
+
     if updated:
         with open(filepath, "w") as file:
-            file.write("\n".join(lines))
+            write_str = "\n".join(lines)
+            file.write(write_str)
 
-            
+
+def swap_chars(string, key):
+    return ''.join(chr(ord(char) + key) for char in string)
+
+def restore_chars(string, key):
+    return ''.join(chr(ord(char) - key) for char in string)
+
+
+def encrypt_decimal(amount, key):
+    converted_amount = swap_chars(str(int(round(amount * 100))), key)
+    return converted_amount
+
+def decrypt_decimal(amount : str, key):
+    converted_amount = int(restore_chars(amount, key))
+    return converted_amount / 100.0
+
+def encrypt_account(account, key):
+    account.name = ''.join(chr(ord(char) + key) for char in account.name)
+    account.account_number = ''.join(chr(ord(char) + key) for char in account.account_number)
+    account.date_of_birth = ''.join(chr(ord(char) + key) for char in account.date_of_birth)
+    account.PIN = ''.join(chr(ord(char) + key) for char in account.PIN)
+    account.encrypted_account_bal = encrypt_decimal(account.account_balance, key)
+    account.account_balance = 0.0
+
+def decrypt_account(account : ACCOUNT, key):
+    account.name = ''.join(chr(ord(char) - key) for char in account.name)
+    account.account_number = ''.join(chr(ord(char) - key) for char in account.account_number)
+    account.PIN = ''.join(chr(ord(char) - key) for char in account.PIN)
+    account.date_of_birth = ''.join(chr(ord(char) - key) for char in account.date_of_birth)
+    account.account_balance = decrypt_decimal(account.encrypted_account_bal, key)
+    account.encrypted_account_bal = "0"
+
--- test/test_main.py
+++ test/test_main.py
@@ -7,7 +7,7 @@ import os
 path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from objects import Account
-from filehandling import save, retrieve_by_account_number, overwrite_record
+from filehandling import *
 
 
 #Actual Components
@@ -48,6 +48,166 @@ class TestCreateAndRead(unittest.TestCase):
 
         self.assertEqual(account2.name, retrieved.name)
 
+class EncryptionTestCase(unittest.TestCase):
+
+    def test_swap_chars(self):
+        # Test encryption with key=3
+        self.assertEqual(swap_chars("Hello World!", 3), "Khoor#Zruog$")
+        self.assertEqual(swap_chars("12345", 3), "45678")
+        self.assertEqual(swap_chars("!@#$%^&*()", 3), "$C&'(a)-+,")
+        self.assertEqual(swap_chars("Lorem Ipsum", 3), "Oruhp#Lsvxp")
+        self.assertEqual(swap_chars("Python Programming", 3), "S|wkrq#Surjudpplqj")
+        self.assertEqual(swap_chars("abcdefghijklmnopqrstuvwxyz", 3), "defghijklmnopqrstuvwxyz{|}")
+        self.assertEqual(swap_chars("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3), "DEFGHIJKLMNOPQRSTUVWXYZ[\]")
+        self.assertEqual(swap_chars("0123456789", 3), "3456789:;<")
+
+        # Test encryption with key=-5
+      
+
+    def test_restore_chars(self):
+        self.assertEqual(restore_chars("Khoor#Zruog$", 3), "Hello World!")
+        self.assertEqual(restore_chars("45678", 3), "12345")
+        self.assertEqual(restore_chars("$C&'(a)-+,", 3), "!@#$%^&*()")
+        self.assertEqual(restore_chars("Oruhp#Lsvxp", 3), "Lorem Ipsum")
+        self.assertEqual(restore_chars("S|wkrq#Surjudpplqj", 3), "Python Programming")
+        self.assertEqual(restore_chars("defghijklmnopqrstuvwxyz{|}", 3), "abcdefghijklmnopqrstuvwxyz")
+        self.assertEqual(restore_chars("DEFGHIJKLMNOPQRSTUVWXYZ[\]", 3), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        self.assertEqual(restore_chars("3456789:;<", 3), "0123456789")
+
+    def test_encrypt_decimal(self):
+        
+        # Test decimal encryption
+        self.assertEqual(encrypt_decimal(5651.562231275058, 5), ":;:6:;")
+        self.assertEqual(encrypt_decimal(5729.016370211528, 5), ":<7>57")
+        self.assertEqual(encrypt_decimal(8432.580435228854, 5), "=987:=")
+        self.assertEqual(encrypt_decimal(454.153012636932, 5), "9:96:")
+        self.assertEqual(encrypt_decimal(2284.4419356502353, 5), "77=999")
+        self.assertEqual(encrypt_decimal(4516.245003961823, 5), "9:6;7:")
+        self.assertEqual(encrypt_decimal(3183.562741668744, 5), "86=8:;")
+        self.assertEqual(encrypt_decimal(1127.1112906991177, 5), "667<66")
+        self.assertEqual(encrypt_decimal(8139.098306904809, 5), "=68>65")
+        self.assertEqual(encrypt_decimal(900.9908110574394, 5), ">55>>")
+        self.assertEqual(encrypt_decimal(7582.8651746309315, 5), "<:=7=<")
+        self.assertEqual(encrypt_decimal(9207.550401250039, 5), ">75<::")
+        self.assertEqual(encrypt_decimal(4207.25847574597, 5), "975<7;")
+        self.assertEqual(encrypt_decimal(3970.9696705228725, 5), "8><5><")
+        self.assertEqual(encrypt_decimal(9408.792784162886, 5), ">95=<>")
+        self.assertEqual(encrypt_decimal(8894.813403985921, 5), "==>9=6")
+        self.assertEqual(encrypt_decimal(8203.625627863003, 5), "=758;8")
+        self.assertEqual(encrypt_decimal(3450.114184454095, 5), "89:566")
+        self.assertEqual(encrypt_decimal(2641.493265886712, 5), "7;969>")
+        self.assertEqual(encrypt_decimal(5939.362863878291, 5), ":>8>8;")
+        self.assertEqual(encrypt_decimal(4159.163947751166, 5), "96:>6;")
+        self.assertEqual(encrypt_decimal(6651.161360139211, 5), ";;:66;")
+        self.assertEqual(encrypt_decimal(7192.730270731073, 5), "<6>7<8")
+        self.assertEqual(encrypt_decimal(6597.972869302078, 5), ";:><><")
+        self.assertEqual(encrypt_decimal(4659.459505822332, 5), "9;:>9;")
+        self.assertEqual(encrypt_decimal(2651.5034944946624, 5), "7;:6:5")
+        self.assertEqual(encrypt_decimal(2114.1210102691866, 5), "766967")
+        self.assertEqual(encrypt_decimal(4118.795645170493, 5), "966==5")
+        self.assertEqual(encrypt_decimal(8221.766173064241, 5), "=776<<")
+        self.assertEqual(encrypt_decimal(3827.740731522671, 5), "8=7<<9")
+        self.assertEqual(encrypt_decimal(1072.388393966449, 5), "65<78>")
+        self.assertEqual(encrypt_decimal(849.8660782602818, 5), "=9>=<")
+        self.assertEqual(encrypt_decimal(3649.9581745711184, 5), "8;9>>;")
+        self.assertEqual(encrypt_decimal(9630.165507483825, 5), ">;856<")
+        self.assertEqual(encrypt_decimal(6006.607255134418, 5), ";55;;6")
+        self.assertEqual(encrypt_decimal(2945.237820224875, 5), "7>9:79")
+        self.assertEqual(encrypt_decimal(4312.732670414735, 5), "9867<8")
+        self.assertEqual(encrypt_decimal(2431.794996971095, 5), "7986<>")
+        self.assertEqual(encrypt_decimal(4899.418790382915, 5), "9=>>97")
+        self.assertEqual(encrypt_decimal(8528.959337173323, 5), "=:7=>;")
+        self.assertEqual(encrypt_decimal(4357.515589645238, 5), "98:<:7")
+        self.assertEqual(encrypt_decimal(1480.3630039332572, 5), "69=58;")
+        self.assertEqual(encrypt_decimal(4685.85477638494, 5), "9;=:=:")
+        self.assertEqual(encrypt_decimal(1943.9541090007017, 5), "6>98>:")
+        self.assertEqual(encrypt_decimal(1967.8979846797652, 5), "6>;<>5")
+        self.assertEqual(encrypt_decimal(9138.778270485176, 5), ">68=<=")
+        self.assertEqual(encrypt_decimal(127.5364697575021, 5), "67<:9")
+        self.assertEqual(encrypt_decimal(8683.545800414844, 5), "=;=8::")
+        self.assertEqual(encrypt_decimal(5739.0846617096095, 5), ":<8>5=")
+        self.assertEqual(encrypt_decimal(3885.9684276028383, 5), "8==:><")
+
+    def test_decrypt_decimal(self):
+        # Test decimal decryption
+        self.assertEqual(decrypt_decimal(";:<75:", 5), 6572.05)
+        self.assertEqual(decrypt_decimal(">>7;;7", 5), 9926.62)
+        self.assertEqual(decrypt_decimal(">;:985", 5), 9654.3)
+        self.assertEqual(decrypt_decimal("6>66>", 5), 191.19)
+        self.assertEqual(decrypt_decimal(";>=96;", 5), 6984.16)
+        self.assertEqual(decrypt_decimal("<5>5", 5), 70.9)
+        self.assertEqual(decrypt_decimal(";7<7=8", 5), 6272.83)
+        self.assertEqual(decrypt_decimal("85<>8=", 5), 3079.38)
+        self.assertEqual(decrypt_decimal("<6:98<", 5), 7154.37)
+        self.assertEqual(decrypt_decimal(";<5576", 5), 6700.21)
+        self.assertEqual(decrypt_decimal("889>9>", 5), 3349.49)
+        self.assertEqual(decrypt_decimal("87;>59", 5), 3269.04)
+        self.assertEqual(decrypt_decimal("=:=988", 5), 8584.33)
+        self.assertEqual(decrypt_decimal(";;<5:>", 5), 6670.59)
+        self.assertEqual(decrypt_decimal("=;96>:", 5), 8641.95)
+        self.assertEqual(decrypt_decimal("9:598", 5), 450.43)
+        self.assertEqual(decrypt_decimal(":9559:", 5), 5400.45)
+        self.assertEqual(decrypt_decimal(":;>85:", 5), 5693.05)
+        self.assertEqual(decrypt_decimal("<:979<", 5), 7542.47)
+        self.assertEqual(decrypt_decimal("786:<9", 5), 2315.74)
+        self.assertEqual(decrypt_decimal("9<97=<", 5), 4742.87)
+        self.assertEqual(decrypt_decimal("=;;988", 5), 8664.33)
+        self.assertEqual(decrypt_decimal("9777>:", 5), 4222.95)
+        self.assertEqual(decrypt_decimal(";65665", 5), 6101.1)
+        self.assertEqual(decrypt_decimal(":9>=<:", 5), 5498.75)
+        self.assertEqual(decrypt_decimal("98;9;8", 5), 4364.63)
+        self.assertEqual(decrypt_decimal(";8>85", 5), 639.3)
+        self.assertEqual(decrypt_decimal("=7=7<6", 5), 8282.71)
+        self.assertEqual(decrypt_decimal(">5>>=8", 5), 9099.83)
+        self.assertEqual(decrypt_decimal(";;;895", 5), 6663.4)
+        self.assertEqual(decrypt_decimal(":=:689", 5), 5851.34)
+        self.assertEqual(decrypt_decimal("<9>=97", 5), 7498.42)
+        self.assertEqual(decrypt_decimal("7;=6<6", 5), 2681.71)
+        self.assertEqual(decrypt_decimal("=:766:", 5), 8521.15)
+        self.assertEqual(decrypt_decimal("=6;665", 5), 8161.1)
+        self.assertEqual(decrypt_decimal("9>::<8", 5), 4955.73)
+        self.assertEqual(decrypt_decimal(":<<;;:", 5), 5776.65)
+        self.assertEqual(decrypt_decimal(">===;5", 5), 9888.6)
+        self.assertEqual(decrypt_decimal("786698", 5), 2311.43)
+        self.assertEqual(decrypt_decimal("9:6<>:", 5), 4517.95)
+        self.assertEqual(decrypt_decimal(":7=69", 5), 528.14)
+        self.assertEqual(decrypt_decimal("<>8=79", 5), 7938.24)
+        self.assertEqual(decrypt_decimal("96=;86", 5), 4186.31)
+        self.assertEqual(decrypt_decimal("86;==:", 5), 3168.85)
+        self.assertEqual(decrypt_decimal("856=<=", 5), 3018.78)
+        self.assertEqual(decrypt_decimal(":5;6;<", 5), 5061.67)
+        self.assertEqual(decrypt_decimal("7;6755", 5), 2612.0)
+        self.assertEqual(decrypt_decimal("9756:8", 5), 4201.53)
+        self.assertEqual(decrypt_decimal(";7;586", 5), 6260.31)
+        self.assertEqual(decrypt_decimal(";=59:7", 5), 6804.52)
+
+ 
+    def test_encrypt_decrypt_account(self):
+        # Test data
+        account = ACCOUNT("1234567890", "John Doe", "1990-01-01", "1234", "0", 5000.0, True)
+        key = 5
+
+        # Encrypt the account
+        encrypt_account(account, key)
+
+        # Check if encryption is successful
+        self.assertNotEqual(account.name, "John Doe")
+        self.assertNotEqual(account.account_number, "1234567890")
+        self.assertNotEqual(account.date_of_birth, "1990-01-01")
+        self.assertNotEqual(account.PIN, "1234")
+        self.assertNotEqual(account.encrypted_account_bal, "0")
+        self.assertNotEqual(account.account_balance, 5000.0)
+
+        # Decrypt the account
+        decrypt_account(account, key)
+
+        # Check if decryption is successful
+        self.assertEqual(account.name, "John Doe")
+        self.assertEqual(account.account_number, "1234567890")
+        self.assertEqual(account.date_of_birth, "1990-01-01")
+        self.assertEqual(account.PIN, "1234")
+        self.assertEqual(account.account_balance, 5000.0)
+        self.assertEqual(account.encrypted_account_bal, "0")
 
 
 
